6c198f1faecc0f093d7a5512c95045423d7b7ac4
Joris Bertier <joris.bertier@gmail.com>
2024-12-28 12:21:06 -0600

    exercice done

--- main.py
+++ main.py
@@ -1,121 +1,27 @@
-class ToolBox:
-    """Boite à outils."""
+class Film:
+    def __init(self, name):
+        self.name = name
 
-    def __init__(self):
-        """Initialise les outils."""
-        self.tools = []
+    def watch(self):
+        print('Bon visionnage')
 
-    def add_tool(self, tool):
-        """Ajoute un outil."""
-        self.tools.append(tool)
+class FilmCassette(Film):
+    """Un film en cassette !"""
 
-    def remove_tool(self, tool):
-        """Enleve un outil."""
-        index = self.tools.index(tool)
-        del self.tools[index]
+    def __init__(self, name):
+        """Initialise le nom et la bande magnetique."""
+        self.name = name
+        self.magnetic_tape = True
+    
+    def rewind(self):
+        """Rembobine le film."""
+        print("C'est long à rembobiner !")
+        self.magnetic_tape = True
+class FilmDVD(Film):
+    def __init__(self, name):
+        self.name = name
+    def display_menu(self):
+        print('Lancer le dvd')
 
-class Screwdriver:
-    """Tournevis."""
-
-    def __init__(self, size=3):
-        """Initialise la taille."""
-        self.size = size
-    
-    def tighten(self, screw):
-        """Serrer une vis."""
-        screw.tighten()
-    
-    def loosen(self, screw):
-        """Desserre une vis."""
-        screw.loosen()
-    
-    def __repr__(self):
-        """Représentation de l'objet."""
-        return f"Tournevis de taille {self.size}"
-
-
-class Hammer:
-    """Marteau."""
-
-    def __init__(self, color="red"):
-        """Initialise la couleur."""
-        self.color = color
-    
-    def paint(self, color):
-        """Paint le marteau."""
-        self.color = color
-    
-    def hammer_in(self, nail):
-        """Enfonce un clou."""
-        nail.nail_in()
-    
-    def remove(self, nail):
-        """Enleve un clou."""
-        nail.remove()
-    
-    def __repr__(self):
-        """Représentation de l'objet."""
-        return f"Marteau de couleur {self.color}"
-
-
-class Screw:
-    MAX_TIGHTNESS = 5
-    
-    def __init__(self):
-        """Initialise son degré de serrage."""
-        self.tightness = 0
-    
-    def loosen(self):
-        """Déserre le vis."""
-        if self.tightness > 0:
-            self.tightness -= 1
-    
-    def tighten(self):
-        """Serre le vis."""
-        if self.tightness < self.MAX_TIGHTNESS:
-            self.tightness += 1
-    
-    def __str__(self):
-        """Retourne une forme lisible de l'objet."""
-        return "Vis avec un serrage de {}".format(self.tightness)
-
-
-class Nail:
-    """Clou."""
-    
-    def __init__(self):
-        """Initialise son statut "dans le mur"."""
-        self.in_wall = False
-    
-    def nail_in(self):
-        """Enfonce le clou dans un mur."""
-        if not self.in_wall:
-            self.in_wall = True
-    
-    def remove(self):
-        """Enlève le clou du mur."""
-        if self.in_wall:
-            self.in_wall = False
-    
-    def __str__(self):
-        """Retourne une forme lisible de l'objet."""
-        wall_state = "dans le mur" if self.in_wall else "hors du mur"
-        return f"Clou {wall_state}."
-    
-toolbox = ToolBox()
-hammer = Hammer()
-screwdriver = Screwdriver()
-screw = Screw()
-nail = Nail()
-
-
-toolbox.add_tool(hammer)
-toolbox.add_tool(screwdriver)
-
-print(screw)
-screwdriver.tighten(screw)
-print(screw)
-
-print(nail)
-hammer.hammer_in(nail)
-print(nail)
+film_dvd = FilmDVD('Blade runner')
+film_dvd.display_menu()
