19bb0148c1f637b9957986d907c07e917070dbb6
Joris Bertier <joris.bertier@gmail.com>
2024-12-28 16:39:21 -0600

    segmentation with module

---
+++ contact/owlContactSystem.py
@@ -0,0 +1,20 @@
+from contact.contactSystem import ContactSystem
+from contact.helpers import varify_address
+
+class OwlContactSystem(ContactSystem):
+    """Envoi un message en utilisant une chouette ! 🧙‍♂️"""
+
+    def __init__(self, address):
+        """Initialise l'addresse."""
+        varify_address(address)
+        self.address = address
+        self.owl = "Hedwige"
+        super().__init__()
+
+    def send(self, message):
+        """Envoi le message."""
+        print(f'Envoi du message "{message}" par chouette {self.owl}')
+
+    def __str__(self):
+        """Représentation."""
+        return f"L'addresse est '{self.address}'"
--- main.py
+++ main.py
@@ -1,158 +1,30 @@
-from abc import ABC
+"""Contacts.
 
-class File(ABC):
-    """Fichier."""
+Séparez le programme en plusieurs modules et packages,
+en ajoutant les fichiers __init__.py et les imports si necessaire.
 
-    def __init__(self, name, size):
-        """Initialise le nom et la taille."""
-        self.name = name
-        self.size = size
-
-    def display(self):
-        """Affiche le fichier."""
-        print(f"Fichier '{self.name}'.")
-
-
-class ImageFile(File):
-    """Fichier image.
-
-    Pas plus à ajouter pour l'instant !
-    """
-    def display(self):
-        return super().display()
-    pass
-
-class Gif(ImageFile):
-    def __init__(self, name, size):
-        self.name =name
-        self.size = size
-
-    def display(self):
-        return super().display()
+Vérifiez que le programme s'éxecute correctement en lancant main.py depuis la racine
+du projet.
+"""
+from contact.user import User
+from contact.owlContactSystem import OwlContactSystem
+from contact.textContactSystem import TextContactSystem
+from contact.helpers import send_mass_messages
 
 
-class Jpg(ImageFile):
-    def __init__(self, name, size):
-        self.name =name
-        self.size = size
 
-    def display(self):
-        return super().display()
+# Our main program.
+def main():
+    alice = User("Alice", TextContactSystem("0102030405"))
+    bob = User("Bob", OwlContactSystem("4 Privet Drive"))
 
-
-class User:
-    """Utilisateur."""
-
-    def __init__(self, username, password):
-        """Initialise le nom d'utilisateur et le mot de passe."""
-        self.username = username
-        self.password = password
-
-    def login(self):
-        """Connecte l'utilisateur."""
-        print(f"L'utilisateur {self.username} est connecté.")
-
-    def post(self, thread, content, file=None):
-        """Poste un message dans un fil de discussion."""
-        if file:
-            post = FilePost(self, "aujourd'hui", content, file)
-        else:
-            post = Post(user=self, time_posted="aujourd'hui", content=content)
-        thread.add_post(post)
-        return post
-
-    def make_thread(self, title, content):
-        """Créé un nouveau fil de discussion."""
-        post = Post(self, "aujourd'hui", content)
-        return Thread(title, "aujourd'hui", post)
-
-    def __str__(self):
-        """représentation de l'utilisateur."""
-        return self.username
-
-
-class Moderator(User):
-    """Utilisateur modérateur."""
-
-    def edit(self, post, content):
-        """Modifie un message."""
-        post.content = content
-
-    def delete(self, thread, post):
-        """Supprime un message."""
-        index = thread.posts.index(post)
-        del thread.posts[index]
-
-
-class Post:
-    """Message."""
-
-    def __init__(self, user, time_posted, content):
-        """Initialise l'utilisateur, la date et le contenu."""
-        self.user = user
-        self.time_posted = time_posted
-        self.content = content
-
-    def display(self):
-        """Affiche le message."""
-        print(f"Message posté par {self.user} le {self.time_posted}:")
-        print(self.content)
-
-
-class FilePost(Post):
-    """Message comportant un fichier."""
-
-    def __init__(self, user, time_posted, content, file):
-        """Initialise le fichier."""
-        self.user = user
-        self.time_posted = time_posted
-        self.content = content
-        self.file = file
-
-    def display(self):
-        """Affiche le contenu et le fichier."""
-        super().display()
-        print("pièce jointe:")
-        self.file.display()
-
-
-class Thread:
-    """Fil de discussions."""
-
-    def __init__(self, title, time_posted, post):
-        """Initialise le titre, la date et les posts.
-
-        Attention ici: on commence par un seul post, celui du sujet.
-        Les réponses à ce post ne pourrons s'ajouter qu'ultérieurement.
-        En effet, on ne créé pas directement un nouveau fil avec des réponses. ;)
-        """
-        self.title = title
-        self.time_posted = time_posted
-        self.posts = [post]
-
-    def display(self):
-        """Affiche le fil de discussion."""
-        print("----- THREAD -----")
-        print(f"titre: {self.title}, date: {self.time_posted}")
-        print()
-        for post in self.posts:
-            post.display()
-            print()
-        print("------------------")
-
-    def add_post(self, post):
-        """Ajoute un post."""
-        self.posts.append(post)
-
-    user = User("John", "superpassword")
-    moderator = Moderator("Lucie", "helloworld")
-
-    cake_thread = user.make_thread("Gâteau à la vanille 🍰 ???", "Vous aimez ou non ?")
-    cake_thread.display()
-
-    moderator.post(cake_thread, content="Oui j'aime beaucoup ! 😚")
-    cake_thread.display()
-
-    irrelevant_post = user.post(cake_thread, content="Et vous aimez les voitures ?")
-    response = moderator.post(cake_thread, content="C'est hors sujet sur ce forum 😕")
-    cake_thread.display()
+    user_list = [alice, bob]
+    send_mass_messages("Hello {name}, Comment vas-tu?", user_list)
+    send_mass_messages(
+        "Salut {name}. Tes informations de contact sont-elles corrects?"
+        " Nous avons celles-ci: {contact_info}.",
+        user_list,
+    )
+    
+if __name__ == "__main__":
+    main()
