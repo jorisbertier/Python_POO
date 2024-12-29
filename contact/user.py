711780211aef168d58772a18602aced3246135ff
Joris Bertier <joris.bertier@gmail.com>
2024-12-29 09:41:59 -0600

    exercice expection done

--- contact/user.py
+++ contact/user.py
@@ -3,9 +3,19 @@ class User:
 
     def __init__(self, name, contact_method):
         """Initialise un nom et une methode de contact."""
+        if len(name) < 6:
+            raise ShortName()
         self.name = name
         self.contact_method = contact_method
 
     def send(self, message):
         """Envoit un message."""
         self.contact_method.send(message)
+
+class ShortName(Exception):
+    def __init__(self, name, *args, **kwargs):
+        msg = f'Name is too short minimum 6 : {name}'
+        super().__init__(msg, *args, **kwargs)
+
+user = User('tesf', 'courrier')
+print(user.name)
